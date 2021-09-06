878edb45a078c20ade1dea8832fc3b14a209b971
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-09-06 10:08:36 +0100

    added Linear Reg

--- lekuto682/trainer.py
+++ lekuto682/trainer.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
 class Trainer():
     def train(self):
         scaler = StandardScaler()
+        model = LinearRegression()
