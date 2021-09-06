d19ed2e5f1968c19fc24d940de29b21b7ce40355
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-09-06 10:06:13 +0100

    added ohe

--- lekuto682/trainer.py
+++ lekuto682/trainer.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 
 class Trainer():
     def train(self):
         scaler = StandardScaler()
+        ohe = OneHotEncoder()
